413f037f0ea286daf561fa1c53748da8219dd024
Yogesh Kamti <yogeshkamti@gmail.com>
2026-01-19 23:02:20 +0545

    Production URL updated

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { useUserStore } from "@/store/userStore";
 
-const BASE_URL = "http://127.0.0.1:8000";
+const PROD_API_URL = "https://westernpharmacy.com.np";
+const DEV_URL = "http://127.0.0.1:8000";
+const BASE_URL = PROD_API_URL;
 
 export const api = axios.create({
   baseURL: `${BASE_URL}/api`,
